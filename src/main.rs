c79592f109740b0dbc925f1e32cd25d675bc440a
Jona Ohme <ohmejona@gmail.com>
2022-11-06 14:32:30 +0100

    displaying text editor without much functionality

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,11 @@
-use std::io::{self, stdout, Read};
-use termion::raw::IntoRawMode;
+mod editor;
+mod terminal;
+
+use editor::Editor;
+pub use terminal::Terminal;
+pub use editor::Position;
 
 fn main() {
-
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("{:?} \r", b);
-        } else {
-            println!("{:?} ({})\r", b ,c);
-        }
-
-        if c == 'q' {
-            break;
-        }
-
-    }
+    Editor::default().run();
 }
+ 
