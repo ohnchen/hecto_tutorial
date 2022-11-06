8e9bd18a402a0f338f7beba64ff2087ed7445785
Jona Ohme <ohmejona@gmail.com>
2022-11-06 15:41:13 +0100

    working document viewer

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,7 +1,5 @@
 use crate::Position;
-
 use std::io::{self, stdout, Write};
-
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
@@ -10,7 +8,6 @@ pub struct Size {
     pub width: u16,
     pub height: u16,
 }
-
 pub struct Terminal {
     size: Size,
     _stdout: RawTerminal<std::io::Stdout>,
@@ -22,16 +19,14 @@ impl Terminal {
         Ok(Self {
             size: Size {
                 width: size.0,
-                height: size.1,
+                height: size.1.saturating_sub(2),
             },
             _stdout: stdout().into_raw_mode()?,
         })
     }
-
     pub fn size(&self) -> &Size {
         &self.size
     }
-
     pub fn clear_screen() {
         print!("{}", termion::clear::All);
     }
@@ -40,16 +35,14 @@ impl Terminal {
     pub fn cursor_position(position: &Position) {
         let Position{mut x, mut y} = position;
         x = x.saturating_add(1);
-        y = y.saturating_sub(1);
+        y = y.saturating_add(1);
         let x = x as u16;
         let y = y as u16;
-        print!("{}", termion::cursor::Goto(x,y));
+        print!("{}", termion::cursor::Goto(x, y));
     }
-
     pub fn flush() -> Result<(), std::io::Error> {
         io::stdout().flush()
     }
-
     pub fn read_key() -> Result<Key, std::io::Error> {
         loop {
             if let Some(key) = io::stdin().lock().keys().next() {
@@ -57,15 +50,12 @@ impl Terminal {
             }
         }
     }
-
     pub fn cursor_hide() {
         print!("{}", termion::cursor::Hide);
     }
-
     pub fn cursor_show() {
         print!("{}", termion::cursor::Show);
     }
-
     pub fn clear_current_line() {
         print!("{}", termion::clear::CurrentLine);
     }
